4192d0bf176c409d18882244df58309653b59462
Maz5710 <marian.arnot@gmail.com>
2023-02-24 16:32:53 +1300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const url = "https://gnews.io/api/v4/";
 
-
 // Get Inputs:  These are Global Variables and should be at the top!
 const countrySelect = document.getElementById('countrySelect');
 const topicSelect = document.getElementById('topicSelect');
 const searchBtn = document.getElementById('searchBtn');
 const searchInput = document.getElementById('searchInput');
+const languageSelect = document.getElementById('languageSelect');
 
 
-// API TOKEN PULL
+// API Toekn Pull
 $.ajax({
     type: 'GET',
     url: './js/config.json',
@@ -23,23 +23,32 @@ $.ajax({
 function mainFunction(myKey) {
 
     const key = myKey;
-    getNews('en', 'breaking-news')
+    getNews('en', 'breaking-news', 'language')
 
-    $('#topicSelect').change(function () {
-        filtered(countrySelect.value, topicSelect.value);
+    $(`#searchSubmit`).click(function () {
+        searchNews(searchInput.value);
+        //console.log('working');
     })
 
-    $('#tcountrySelect').change(function () {
-        filtered(countrySelect.value, topicSelect.value);
+    $('#topicSelect').change(function () {
+        filtered(countrySelect.value, topicSelect.value, languageSelect);
+    })
+
+    $('#countrySelect').change(function () {
+        filtered(countrySelect.value, topicSelect.value, languageSelect);
+    })
+
+    $('#languageSelect').click(function () {
+        filtered(countrySelect.value, topicSelect.value, languageSelect);
     })
 
     function filtered() {
-        getNews(countrySelect.value, topicSelect.value)
+        getNews(countrySelect.value, topicSelect.value, languageSelect.value)
     }
 
-    function getNews(country, topic) {
+    function getNews(country, topic, language) {
         $.ajax({
-            url: `${url}top-headlines?category=${topic}&lang=en&country=${country}&apikey=${key}`,
+            url: `${url}top-headlines?category=${topic}&lang=${language}&country=${country}&apikey=${key}`,
             type: 'GET',
             data: 'json',
 
@@ -50,28 +59,57 @@ function mainFunction(myKey) {
                 let i = 0;
                 for (i = 0; i < newsData.articles.length; i++) {
                     let story = newsData.articles[i];
-                    console.log(story);
-
+                    //console.log(story);
                     results.innerHTML +=
-
-                        '<div class="col-lg-4 col-md-6 col-sm-12" > ' +
-                        '<div class="card mt-3">' +
-                        '<img src="' + story.image + '" class="card-img-top" alt="news image">' +
-                        '<div class="card-body">' +
-                        '<h5 class="card-title fw-bold">' + story.title + '</h5>' +
-                        '<p class="card-text fw-light"> ' + story.description + '<br>' + '<br>' + 'Source: ' + '<a class="text-primary" href="' + story.source.url + '" target="_blank">' + story.source.name + '</a>' + '<br>' + '<br>' + '<a class="text-danger" href="' + story.url + '" target="_blank">' + 'View full article' + '</a>' + '</p>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>';
-
+                    `<div class="col-lg-4 col-md-6 col-sm-12" >
+                        <div class="card mt-3">
+                            <img src="${story.image}" class="card-img-top" alt="news image">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">${story.title}</h5>
+                                <p class="card-text">${story.description}<br><br>Source: <a class="text-success" href="${story.source.url}" target="_blank">${story.source.name}</a><br><br><button class="btn-primary btn"><a class="text-light" href="${story.url} " target="_blank">View full article</a></button></p> 
+                            </div>
+                        </div>
+                    </div>`;
                 }
             },
 
             error: function () {
                 console.log('error - GET NEWS function');
             }
-
         })
-
     }
+
+
+    function searchNews(search) {
+        $.ajax({
+            url: `${url}/search?q=${search}&lang=en&apikey=${key}`,
+            type: 'GET',
+            data: 'json',
+            success: function (newsData) {
+                let results = document.getElementById('results');
+                results.innerHTML = '';
+                let i = 0;
+                for (i=0; i <newsData.articles.length; i++) {
+                    let story = newsData.articles[i];
+                   // console.log(story);    
+                    results.innerHTML += 
+                    `<div class="col-lg-4 col-md-6 col-sm-12" >
+                        <div class="card mt-3">
+                            <img src="${story.image}" class="card-img-top" alt="news image">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">${story.title}</h5>
+                                <p class="card-text">${story.description}<br><br>Source: <a class="text-success" href="${story.source.url}" target="_blank">${story.source.name}</a><br><br><button class="btn-primary btn"><a class="text-light" href="${story.url} " target="_blank">View full article</a></button></p> 
+                            </div>
+                        </div>
+                    </div>`;
+                }
+            },
+
+            error: function() {
+                console.log('error - GET NEWS function');
+            }
+        })
+    }
+
 }
+
